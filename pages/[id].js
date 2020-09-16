751af3a63469c1aa541a87600f2c00c1261a3c03
kibrahi3 <53925255+kibrahi3@users.noreply.github.com>
2020-09-16 19:41:54 -0400

    Add files via upload

---
+++ pages/[id].js
@@ -0,0 +1,37 @@
+
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  )
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return [
+    {
+      params: {
+        // Statically Generates /posts/a/b/c
+        id: ['a', 'b', 'c']
+      }
+    }
+    //...
+  ]
+}
+
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
